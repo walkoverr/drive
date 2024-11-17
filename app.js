03171b447b458db038745642e56e89681951bcf7
abhishek yadav <abhisathayadav143@gmail.com>
2024-11-17 17:09:58 +0530

    updated

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const userRouter = require('./routes/user.routes')
 app.set("view engine","ejs");
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 app.use('/user',userRouter);
 
 app.listen(3000,()=>{
     console.log("port is listening")
-})
+})  
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,31 @@
 const express = require('express')
 
 const router = express.Router();
+const{body, validationResult}= require('express-validator')
 
-router.get('/test',(req,res)=>{
-    res.send("i am testing my route")
+router.get('/register',(req,res)=>{
+    res.render('register');
 })
+router.post('/register',
+    body('email').trim().isEmail().isLength({min:13}),
+    body('password').trim().isLength({min:5}),
+    body('username').trim().isLength({min:3})
+,(req,res)=>{
+    const error = validationResult(req);
+    
+    if(!error.isEmpty())
+    {
+        return res.status(400).json({
+            error:error.array(),
+            message:"invalid "
+        })
+    }
+     
 
+    
+    // console.log(error);
+    // res.send(error);   
+    // console.log(req.body.username)
+    // res.send('user registered')
+})  
 module.exports= router;
