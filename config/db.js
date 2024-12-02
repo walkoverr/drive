a697685a563fdf9641c5f1bd2fc882586ef3037f
abhishek yadav <abhisathayadav143@gmail.com>
2024-12-02 22:21:07 +0530

    new

--- config/db.js
+++ config/db.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose')
 const dotenv= require('dotenv')
 function connectToDB()
 {
-    const uri = process.env.MONGO_URI;
-    console.log(uri);
-    if(!uri)
-    {
-        console.log("ERROR")
-        process.exit(1);
-    }
+    // const uri = process.env.MONGO_URI;
+    // console.log(uri);
+    // if(!uri)
+    // {
+    //     console.log("ERROR")
+    //     process.exit(1);
+    // }
+    const uri = "mongodb://127.0.0.1/my-drive"
     mongoose.connect(uri).then(()=>{
         console.log("Connected To DB")
     })
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const user= require('../models/user.model')
 const mongoose = require("mongoose")
 const router = express.Router();
+const bcrypt = require("bcrypt")
 const{body, validationResult}= require('express-validator')
 
 router.get('/home',(req,res)=>{
@@ -14,7 +15,7 @@ router.post('/register',
     body('email').trim().isEmail().isLength({min:13}),
     body('password').trim().isLength({min:5}),
     body('username').trim().isLength({min:3})
-,(req,res)=>{
+,async(req,res)=>{
     const error = validationResult(req);
     
     if(!error.isEmpty())
@@ -27,11 +28,35 @@ router.post('/register',
     // console.log(req.body)
     // res.send('user registered')
     const {email,username,password}= req.body
-    const newUser = user.create({
+    const hashpass = await bcrypt.hash(password,10);
+    const newUser =await user.create({
         email:email,
         username:username,
-        password:password,
+        password:hashpass,
     })
-    res.json(newUser);
-})  
+    console.log(newUser);
+    res.json({newUser});
+}) 
+
+router.get('/loginpage',(req,res)=>
+    {
+        res.render("login");   
+    })
+router.post('/loginpage',async(req,res)=>
+    {
+        const {username,password}= req.body
+        const exi =  await user.findOne({username:username})
+        if(!exi)
+        {
+            return res.status(404).json({msg:"user not found pls register"});
+        }
+        // console.log(await bcrypt.hash(password,10))
+        // console.log(exi.password)
+       const  match = await bcrypt.compare(password,exi.password)
+       console.log(match)
+       if(!match){
+        return res.status(404).json({msg:"pls give correct details"});
+       }
+        return res.render("home")
+    })
 module.exports= router;
